f4076cc68d7ba507d7b3c4c5752f30d663f6645a
MatsAmundsen <matsgab@gmail.com>
2026-08-26 17:15:32 +0000

    Fix: avinstaller gammel PWA og naviger klientene til fersk side.

--- sw.js
+++ sw.js
@@ -1,13 +1,36 @@
-/* Matteguiden: ikke cache. Avinstaller deg selv. */
-self.addEventListener('install', function () {
-    self.skipWaiting();
+/* Matteguiden: ikke cache. Avinstaller deg selv og last inn fersk side. */
+self.addEventListener("install", function () {
+  self.skipWaiting();
 });
-self.addEventListener('activate', function (event) {
-    event.waitUntil(
-        caches.keys().then(function (keys) {
-            return Promise.all(keys.map(function (k) { return caches.delete(k); }));
-        }).then(function () {
-            return self.registration.unregister();
-        })
-    );
+self.addEventListener("activate", function (event) {
+  event.waitUntil(
+    caches
+      .keys()
+      .then(function (keys) {
+        return Promise.all(keys.map(function (k) { return caches.delete(k); }));
+      })
+      .then(function () {
+        return self.clients.claim();
+      })
+      .then(function () {
+        return self.registration.unregister();
+      })
+      .then(function () {
+        return self.clients.matchAll({ type: "window" });
+      })
+      .then(function (clients) {
+        return Promise.all(
+          clients.map(function (c) {
+            var url = c.url || "/";
+            try {
+              var u = new URL(url, self.location.origin);
+              u.searchParams.set("mg", String(Date.now()));
+              return c.navigate(u.pathname + u.search + u.hash);
+            } catch (e) {
+              return c.navigate(url);
+            }
+          })
+        );
+      })
+  );
 });
